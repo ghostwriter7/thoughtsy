de8b9ec3ecba7f8520ad187962e6dcb545da9988
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-07-20 20:29:13 +0200

    TypeORM initialized, connection to PostgreSQL established

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {environment} from '../environments/environment';
 import {ThoughtsModule} from './thoughts/thoughts.module';
 
 @Module({
-  imports: [ThoughtsModule],
+  imports: [ThoughtsModule,
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: environment.DB_USER,
+    password: environment.DB_PASSWORD,
+    database: environment.DB_NAME,
+    entities: [],
+    synchronize: true
+  })],
   controllers: [],
   providers: [],
 })
